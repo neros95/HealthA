c0d2311920942637695b0ded1ed4d2982b04bc68
Pacho <andreas.petersson0312@stud.hkr.se>
2020-05-22 17:48:33 +0200

    Update main activity sign in logic

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -7,27 +7,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.myapplication3.firebase.FirebaseAuthentication;
 import com.example.myapplication3.ui.CreateAccount;
+import com.google.android.gms.tasks.Tasks;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button, button1;
+    EditText email, password;
+
+    Intent myIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.logIn);
+        button1 = findViewById(R.id.createAccount);
 
-        button1= findViewById(R.id.createAccount);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        boolean authState = FirebaseAuthentication.firebaseAuth.authState();
+        if (authState) {
+            goToMain2();
+        }
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(myIntent);
+                if (FirebaseAuthentication.firebaseAuth.signInExistingUser(email.getText().toString(), password.getText().toString())) {
+                    goToMain2();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Login failed. Incorrect email or password",Toast.LENGTH_SHORT).show();
+                    email.setText("");
+                    password.setText("");
+                }
             }
         });
        button1.setOnClickListener(new View.OnClickListener() {
@@ -36,8 +56,12 @@ public class MainActivity extends AppCompatActivity {
                FragmentManager fragmentManager= getSupportFragmentManager();
                CreateAccount createAccount = new CreateAccount();
                fragmentManager.beginTransaction().replace(R.id.main, createAccount).commit();
-
            }
        });
+    }
+
+    private void goToMain2() {
+        myIntent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(myIntent);
     }
 }
