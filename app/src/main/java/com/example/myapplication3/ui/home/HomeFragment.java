33d60e16d818cc29b957ff81aac1016827d191c9
neros95 <pao_9517@hotmail.com>
2020-05-27 15:15:37 +0200

    bmi funktion

--- app/src/main/java/com/example/myapplication3/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication3/ui/home/HomeFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +18,10 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.myapplication3.R;
 
 public class HomeFragment extends Fragment {
+
+    EditText height, weight;
+    TextView result;
+    Button calculateBmi;
 
     private HomeViewModel homeViewModel;
 
@@ -31,7 +38,42 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        height.findViewById(R.id.heigh);
+        weight.findViewById(R.id.weight);
+        result.findViewById(R.id.result);
+        calculateBmi.findViewById(R.id.calculateBMI);
+
+
+        calculateBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmi();
+            }
+        });
+
+
 
         return root;
+    }
+
+    private void bmi(){
+        float a, b, c;
+        a = Float.parseFloat(height.getText().toString())/100;
+        b= Float.parseFloat(weight.getText().toString());
+        c= b / (a * a);
+
+        result.setText("Your ideal weight" +c);
+
+        if (c<=18.8){
+            Toast.makeText(getContext(),"You are underweight",Toast.LENGTH_LONG);
+        }
+        if ((c>= 18.8) && (c<25)) {
+
+            Toast.makeText(getContext(),"Normal weight", Toast.LENGTH_LONG);
+
+        }
+        if (c>=25){
+            Toast.makeText(getContext(),"Normal weight", Toast.LENGTH_LONG);
+        }
     }
 }
